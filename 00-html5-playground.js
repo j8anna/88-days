3725c62ce46a95f8c546ce18ec04e4f69a34c461
Joanna Górecka <j8anna@users.noreply.github.com>
2017-06-20 14:24:36 +0200

    small HTML5/CSS3 project :)

---
+++ 00-html5-playground.js
@@ -0,0 +1,4 @@
+function visitMyGithub() {
+    name = document.forms[0].name.value;
+    alert("Hi " + name.toUpperCase() + ", thanks for visiting my page. You will now be redirected to my GitHub.");
+}
